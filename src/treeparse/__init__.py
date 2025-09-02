381ee769a61d56603ad7f5a65a9a07bbea3a7a06
wr1 <8971152+wr1@users.noreply.github.com>
2025-09-02 08:27:46 +0200

    Update __init__.py with new imports

--- src/treeparse/__init__.py
+++ src/treeparse/__init__.py
@@ -1,18 +1,18 @@
-from .argument import argument
-from .option import option
-from .command import command
-from .chain import Chain
-from .group import group
-from .cli import cli
-from .color_config import color_config
+from .core.argument import argument
+from .core.option import option
+from .core.command import command
+from .core.chain import chain
+from .core.group import group
+from .core.cli import cli
+from .utils.color_config import color_config
 
 # Rebuild models to handle forward references
 argument.model_rebuild()
 option.model_rebuild()
 command.model_rebuild()
-Chain.model_rebuild()
+chain.model_rebuild()
 group.model_rebuild()
 cli.model_rebuild()
 color_config.model_rebuild()
 
-__all__ = ["argument", "option", "command", "Chain", "group", "cli", "color_config"]
+__all__ = ["argument", "option", "command", "chain", "group", "cli", "color_config"]
