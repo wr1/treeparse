a3d67d0d713f0d2113b1bd8b45fd4be05d9bdaf9
wr1 <8971152+wr1@users.noreply.github.com>
2025-12-19 00:33:55 +0100

    fix boolean handling

--- src/treeparse/core/cli.py
+++ src/treeparse/core/cli.py
@@ -27,7 +27,7 @@ def str2bool(v):
     if v.lower() in ("yes", "true", "t", "y", "1"):
         return True
     elif v.lower() in ("no", "false", "f", "n", "0"):
-        raise argparse.ArgumentTypeError("Boolean value expected.")
+        return False
     else:
         raise argparse.ArgumentTypeError("Boolean value expected.")
 
