5fcbcb16221485070ce03148f2d1d759ec332b84
wr1 <8971152+wr1@users.noreply.github.com>
2026-03-05 15:24:27 +0100

    export CliRunner and CliResult (future.md)

--- src/treeparse/__init__.py
+++ src/treeparse/__init__.py
@@ -7,6 +7,7 @@ from .models.chain import chain
 from .models.group import group
 from .models.cli import cli
 from .utils.color_config import color_config
+from .testing import CliRunner, CliResult
 
 # Rebuild models to handle forward references
 argument.model_rebuild()
@@ -17,4 +18,14 @@ group.model_rebuild()
 cli.model_rebuild()
 color_config.model_rebuild()
 
-__all__ = ["argument", "option", "command", "chain", "group", "cli", "color_config"]
+__all__ = [
+    "argument",
+    "option",
+    "command",
+    "chain",
+    "group",
+    "cli",
+    "color_config",
+    "CliRunner",
+    "CliResult",
+]
